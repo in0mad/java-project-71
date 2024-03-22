1a47e5597dfa979c4fde4ac73f850a4ea51e6810
Sergey Sukhanov <ss.sukhanoff@gmail.com>
2024-03-22 20:20:41 +0300

    updated a CI file + connected a testLogger plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,8 @@ plugins {
     application
     `java-library`
     id("checkstyle")
+    id("com.adarshr.test-logger") version "4.0.0"
+
 }
 
 group = "hexlet.code"
@@ -14,15 +16,16 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation(platform("org.junit:junit-bom:5.10.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-    implementation("org.apache.commons:commons-lang3:3.12.0")
+    implementation("org.apache.commons:commons-lang3:3.14.0")
     implementation("org.apache.commons:commons-collections4:4.4")
     implementation("info.picocli:picocli:4.7.5")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.17.0")
     implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-xml:2.17.0")
     implementation("com.puppycrawl.tools:checkstyle:10.14.2")
+
 
 
 }
@@ -35,10 +38,10 @@ tasks.getByName("run", JavaExec::class) {
     standardInput = System.`in`
 }
 
-tasks.getByName("run", JavaExec::class) {
-    standardInput = System.`in`
-}
-
 application {
     mainClass = "hexlet.code.App"
+}
+
+testlogger {
+    showFullStackTraces = true
 }
