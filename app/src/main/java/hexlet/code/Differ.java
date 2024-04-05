3e3ea0cb46335847d7acabb889fe50020d8cb6ab
Sergey Sukhanov <ss.sukhanoff@gmail.com>
2024-04-05 08:47:59 +0300

    fixed minor issues

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,18 +1,13 @@
 package hexlet.code;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
-
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Map;
 
 public class Differ {
     public static String generate(String filepath1, String filepath2, String format) throws Exception {
-        Path absolutePath1 = Paths.get(filepath1).toAbsolutePath().normalize();
-        Path absolutePath2 = Paths.get(filepath2).toAbsolutePath().normalize();
+        String absolutePath1 = Paths.get(filepath1).toAbsolutePath().normalize().toString();
+        String absolutePath2 = Paths.get(filepath2).toAbsolutePath().normalize().toString();
 
         String dataFile1 = readFile(absolutePath1);
         String dataFile2 = readFile(absolutePath2);
@@ -20,20 +15,19 @@ public class Differ {
         Map<String, Object> parseFile1 = Parser.parse(dataFile1, defineFormat(absolutePath1));
         Map<String, Object> parseFile2 = Parser.parse(dataFile2, defineFormat(absolutePath2));
 
-
         Map<String, String> keyInfo = Comparator.collectKeyStatus(parseFile1, parseFile2);
 
         return Formatter.format(parseFile1, parseFile2, keyInfo, format);
     }
 
-    public static String readFile(Path filepath) throws Exception {
-        return Files.readString(filepath);
+    public static String readFile(String filepath) throws Exception {
+        return Files.readString(Paths.get(filepath));
     }
 
-    public static String defineFormat(Path filepath) {
-        if (filepath.toString().endsWith(".yaml") || filepath.toString().endsWith(".yml")) {
+    public static String defineFormat(String filepath) {
+        if (filepath.endsWith(".yaml") || filepath.endsWith(".yml")) {
             return  "yaml";
-        } else if (filepath.toString().endsWith(".json")) {
+        } else if (filepath.endsWith(".json")) {
             return  "json";
         } else {
             return "unknown";
