43f62e8e60d77d43f242e59896fac7c842329d79
Sergey Sukhanov <ss.sukhanoff@gmail.com>
2024-04-10 09:53:19 +0300

    refactoring 2.6 + minor issues + test correct

--- app/src/main/java/hexlet/code/Comparator.java
+++ app/src/main/java/hexlet/code/Comparator.java
@@ -1,16 +1,24 @@
 package hexlet.code;
 
-import java.util.*;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.TreeSet;
+import java.util.LinkedList;
 
 public class Comparator {
-    public static Map<String, Map<String, Object>> collectKeyStatus(Map<String, Object> dataFile1, Map<String, Object> dataFile2) {
+    public static Map<String, Map<String, Object>> collectKeyStatus(Map<String, Object> dataFile1,
+                                                                    Map<String, Object> dataFile2) {
         Set<String> unionKeySet = new TreeSet<>(dataFile1.keySet());
         unionKeySet.addAll(dataFile2.keySet());
 
-        Map <String, Map<String, Object>> keyStatusMap = new TreeMap<>();
+        Map<String, Map<String, Object>> keyStatusMap = new TreeMap<>();
 
         unionKeySet.forEach(key -> keyStatusMap.compute(key, (k, v) -> {
-            LinkedList<String> statuses = new LinkedList<>(List.of("Current value", "Key status", "Old value", "New value"));
+            LinkedList<String> statuses = new LinkedList<>(
+                    List.of("Current value", "Key status", "Old value", "New value"));
             Map<String, Object> temp = new HashMap<>();
             for (String status : statuses) {
                 switch (status) {
@@ -35,11 +43,11 @@ public class Comparator {
         Object returned;
         returned = dataFile1.containsKey(key) && !dataFile2.containsKey(key) ? "removed" : "added";
         if (dataFile1.containsKey(key) && dataFile2.containsKey(key)) {
-        valueFile1 = dataFile1.get(key);
-        valueFile2 = dataFile2.get(key);
-        returned = valueFile1 == null || valueFile2 == null
-                ? valueFile1 == valueFile2 ? "unchanged" : "updated"
-                : valueFile1.equals(valueFile2) ? "unchanged" : "updated";
+            valueFile1 = dataFile1.get(key);
+            valueFile2 = dataFile2.get(key);
+            returned = valueFile1 == null || valueFile2 == null
+                    ? valueFile1 == valueFile2 ? "unchanged" : "updated"
+                    : valueFile1.equals(valueFile2) ? "unchanged" : "updated";
         }
 
         return returned;
---
+++ app/src/test/java/hexlet/code/MainTest.java
@@ -0,0 +1,102 @@
+package hexlet.code;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public final class MainTest {
+    private final String resourceDirectory = Paths.get("src", "test", "resources").toString();
+    private String toJson1;
+    private String toJson2;
+    private String toYaml1;
+    private String toYaml2;
+    private String stylishResult;
+    private String plainResult;
+    private String jsonResult;
+    private String defaultResult;
+
+
+    @BeforeEach
+    public void beforeEach() throws Exception {
+        assertTrue(resourceDirectory.endsWith("src/test/resources"));
+        toJson1 = Paths.get(resourceDirectory, "json1.json").toString();
+        toJson2 = Paths.get(resourceDirectory, "json2.json").toString();
+        toYaml1 = Paths.get(resourceDirectory, "yaml1.yml").toString();
+        toYaml2 = Paths.get(resourceDirectory, "yaml2.yml").toString();
+        stylishResult = Files.readString(Paths.get(resourceDirectory, "result-stylish.txt"));
+        plainResult = Files.readString(Paths.get(resourceDirectory, "result-plain.txt"));
+        jsonResult = Files.readString(Paths.get(resourceDirectory, "result-json.txt"));
+        defaultResult = Files.readString(Paths.get(resourceDirectory, "result-default.txt"));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"json", "yaml"})
+    public void stylishTest(String extension) throws Exception {
+        String actual;
+        if (extension.equals("json")) {
+            actual = Differ.generate(toJson1, toJson2, "stylish");
+        } else {
+            actual = Differ.generate(toYaml1, toYaml2, "stylish");
+        }
+        String expected = stylishResult;
+        assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"json", "yaml"})
+    public void plainTest(String extension) throws Exception {
+        String actual;
+        if (extension.equals("json")) {
+            actual = Differ.generate(toJson1, toJson2, "plain");
+        } else {
+            actual = Differ.generate(toYaml1, toYaml2, "plain");
+        }
+        String expected = plainResult;
+        assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"json", "yaml"})
+    public void jsonTest(String extension) throws Exception {
+        String actual;
+        if (extension.equals("json")) {
+            actual = Differ.generate(toJson1, toJson2, "json");
+        } else {
+            actual = Differ.generate(toYaml1, toYaml2, "json");
+        }
+        String expected = jsonResult;
+        assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"json", "yaml"})
+    public void defaultTest(String extension) throws Exception {
+        String actual;
+        if (extension.equals("json")) {
+            actual = Differ.generate(toJson1, toJson2, "default");
+        } else {
+            actual = Differ.generate(toYaml1, toYaml2, "default");
+        }
+        String expected = defaultResult;
+        assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"json", "yaml"})
+    public void defaultTest2(String extension) throws Exception {
+        String actual;
+        if (extension.equals("json")) {
+            actual = Differ.generate(toJson1, toJson2);
+        } else {
+            actual = Differ.generate(toYaml1, toYaml2);
+        }
+        String expected = defaultResult;
+        assertEquals(expected, actual);
+    }
+}
