62aa23f0177eff769e15f9b6f5c1f285e4df7c71
Sergey Sukhanov <ss.sukhanoff@gmail.com>
2024-04-14 11:03:39 +0300

    refactoring 3.6 Change a logic of comparator updating

--- app/src/main/java/hexlet/code/Comparator.java
+++ app/src/main/java/hexlet/code/Comparator.java
@@ -1,58 +1,76 @@
 package hexlet.code;
 
-import java.util.*;
+
+import java.util.Map;
+import java.util.Set;
+import java.util.List;
+import java.util.HashMap;
+import java.util.TreeSet;
+import java.util.ArrayList;
+
 
 public class Comparator {
     public static List<Map<String, Object>> collectKeyStatus(Map<String, Object> dataFile1,
-                                                                    Map<String, Object> dataFile2) {
+                                                             Map<String, Object> dataFile2) {
         Set<String> unionKeySet = new TreeSet<>(dataFile1.keySet());
         unionKeySet.addAll(dataFile2.keySet());
 
         List<Map<String, Object>> keyStatusMap = new ArrayList<>();
 
-        unionKeySet.forEach(key -> keyStatusMap.compute(key, (k, v) -> {
-            LinkedList<String> statuses = new LinkedList<>(
-                    List.of("Key value", "Key status", "Old value", "New value"));
-            Map<String, Object> temp = new LinkedHashMap<>();
-            for (String status : statuses) {
-                switch (status) {
-                    case "Key value" -> temp.put("Key value", keyVal(key, dataFile1, dataFile2));
-                    case "Key status" -> temp.put("Key status", takeKeyStatus(key, dataFile1, dataFile2));
-                    case "Old value" -> temp.put("Old value", takeOldVal(key, dataFile1, dataFile2));
-                    default -> temp.put("New value", takeNewVal(key, dataFile2));
-                }
-            }
-            return temp;
-        }));
+        unionKeySet.forEach(key -> {
+            Map<String, Object> temp = new HashMap<>();
+            temp.put("KEY", key);
+            temp.putAll(takeDifference(key, dataFile1, dataFile2));
+            keyStatusMap.add(temp);
+        });
         return keyStatusMap;
     }
 
-    public static Object keyVal(String key, Map<String, Object> dataFile1, Map<String, Object> dataFile2) {
-        return dataFile1.getOrDefault(key, dataFile2.get(key));
-    }
 
-    public static Object takeKeyStatus(String key, Map<String, Object> dataFile1, Map<String, Object> dataFile2) {
+    public static Map<String, Object> takeDifference(String key, Map<String, Object> dataFile1,
+                                                     Map<String, Object> dataFile2) {
         Object valueFile1;
         Object valueFile2;
-        Object returned;
-        returned = dataFile1.containsKey(key) && !dataFile2.containsKey(key) ? "removed" : "added";
+        Map<String, Object> returnedMap = new HashMap<>();
         if (dataFile1.containsKey(key) && dataFile2.containsKey(key)) {
             valueFile1 = dataFile1.get(key);
             valueFile2 = dataFile2.get(key);
             if (valueFile1 == null || valueFile2 == null) {
-                returned = valueFile1 == valueFile2 ? "unchanged" : "updated";
+                returnedMap.putAll(nullHandler(valueFile1, valueFile2, key));
             } else {
-                returned = valueFile1.equals(valueFile2) ? "unchanged" : "updated";
+                if (valueFile1.equals(valueFile2)) {
+                    returnedMap.put("KEY STATUS", "unchanged");
+                    returnedMap.put("OLD VALUE", valueFile1);
+                } else {
+                    returnedMap.put("KEY STATUS", "updated");
+                    returnedMap.put("NEW VALUE", valueFile2);
+                }
             }
         }
-        return returned;
+        if (dataFile1.containsKey(key) && !dataFile2.containsKey(key)) {
+            valueFile1 = dataFile1.get(key);
+            returnedMap.put("KEY STATUS", "removed");
+            returnedMap.put("OLD VALUE", valueFile1);
+        } else {
+            valueFile2 = dataFile2.get(key);
+            returnedMap.put("KEY STATUS", "added");
+            returnedMap.put("NEW VALUE", valueFile2);
+        }
+        return returnedMap;
     }
 
-    public static Object takeOldVal(String key, Map<String, Object> dataFile1, Map<String, Object> dataFile2) {
-        return dataFile1.getOrDefault(key, dataFile2.get(key));
-    }
-
-    public static Object takeNewVal(String key, Map<String, Object> dataFile2) {
-        return dataFile2.getOrDefault(key, "");
+    public static Map<String, Object> nullHandler(Object valueFileOld, Object valueFileNew, String key) {
+        Map<String, Object> returnedMap = new HashMap<>();
+        if (valueFileOld == null && valueFileNew != null) {
+            returnedMap.put("KEY STATUS", "updated");
+            returnedMap.put("NEW VALUE", valueFileNew);
+        } else if (valueFileOld != null && valueFileNew == null) {
+            returnedMap.put("KEY STATUS", "removed");
+            returnedMap.put("NEW VALUE", valueFileOld);
+        } else {
+            returnedMap.put("KEY STATUS", "unchanged");
+            returnedMap.put("OLD VALUE", valueFileOld);
+        }
+        return returnedMap;
     }
 }
