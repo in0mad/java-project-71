73a5ba2563573239cbd01f89b75e3174b975082f
Sergey Sukhanov <ss.sukhanoff@gmail.com>
2024-04-18 11:29:19 +0300

    refactoring 4.4 Removed a null checker from everywhere

--- app/src/main/java/hexlet/code/Comparator.java
+++ app/src/main/java/hexlet/code/Comparator.java
@@ -7,6 +7,7 @@ import java.util.Set;
 import java.util.TreeSet;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
+import java.util.Objects;
 
 public class Comparator {
     public static List<Map<String, Object>> collectKeyStatus(Map<String, Object> dataFile1,
@@ -31,43 +32,26 @@ public class Comparator {
         Object valueFile1;
         Object valueFile2;
         Map<String, Object> returnedMap = new LinkedHashMap<>();
-        if (dataFile1.containsKey(key) && dataFile2.containsKey(key)) {
-            valueFile1 = dataFile1.get(key);
-            valueFile2 = dataFile2.get(key);
-            if (valueFile1 == null || valueFile2 == null) {
-                returnedMap.putAll(nullHandler(valueFile1, valueFile2));
-            } else {
-                if (valueFile1.equals(valueFile2)) {
-                    returnedMap.put("KEY STATUS", "unchanged");
-                    returnedMap.put("OLD VALUE", valueFile1);
-                } else {
-                    returnedMap.put("KEY STATUS", "updated");
-                    returnedMap.put("OLD VALUE", valueFile1);
-                    returnedMap.put("NEW VALUE", valueFile2);
-                }
-            }
-        } else if (dataFile1.containsKey(key) && !dataFile2.containsKey(key)) {
+        if (!dataFile2.containsKey(key)) {
             valueFile1 = dataFile1.get(key);
             returnedMap.put("KEY STATUS", "removed");
             returnedMap.put("OLD VALUE", valueFile1);
-        } else {
+        } else if (!dataFile1.containsKey(key)){
             valueFile2 = dataFile2.get(key);
             returnedMap.put("KEY STATUS", "added");
             returnedMap.put("NEW VALUE", valueFile2);
+        } else {
+            valueFile1 = dataFile1.get(key);
+            valueFile2 = dataFile2.get(key);
+            if (Objects.equals(valueFile1, valueFile2)) {
+                returnedMap.put("KEY STATUS", "unchanged");
+                returnedMap.put("OLD VALUE", valueFile1);
+            } else {
+                returnedMap.put("KEY STATUS", "updated");
+                returnedMap.put("OLD VALUE", valueFile1);
+                returnedMap.put("NEW VALUE", valueFile2);
+            }
         }
         return returnedMap;
-    }
-
-    public static Map<String, Object> nullHandler(Object valueFileOld, Object valueFileNew) {
-        Map<String, Object> tempMap = new LinkedHashMap<>();
-        if (valueFileOld != valueFileNew) {
-            tempMap.put("KEY STATUS", "updated");
-            tempMap.put("OLD VALUE", valueFileOld);
-            tempMap.put("NEW VALUE", valueFileNew);
-        } else {
-            tempMap.put("KEY STATUS", "unchanged");
-            tempMap.put("OLD VALUE", valueFileOld);
-        }
-        return tempMap;
     }
 }
